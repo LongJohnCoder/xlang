e11fb65c1564e83f4c1f684b183927ba18be4dad
Harry Pierson <hpierson@microsoft.com>
2019-01-15 20:49:34 -0800

    PEGTL hello world

--- src/tool/idl3/main.cpp
+++ src/tool/idl3/main.cpp
@@ -4,18 +4,36 @@ struct writer : xlang::text::writer_base<writer>
 {
 };
 
-int main(int const argc, char** argv)
+namespace pegtl = tao::TAO_PEGTL_NAMESPACE;
+
+namespace hello
 {
+    struct prefix : pegtl::string<'H', 'e', 'l', 'l', 'o',',',' '>{};
+    struct name : pegtl::plus<pegtl::alpha>{};
+    struct grammar : pegtl::must<prefix, name, pegtl::one<'!'>, pegtl::eof>{};
+
+    template<typename Rule>
+    struct action : pegtl::nothing<Rule>{};
+
+    template<>
+    struct action<name>
+    {
+        template<typename Input>
+        static void apply(Input const& in, std::string& v)
+        {
+            v = in.string();
+        }
+    };
+}
+
+int main(int const /*argc*/, char** /*argv*/)
+{
+    pegtl::string_input<> in("Hello, Harry!", "from_content");
+
+    std::string name;
+    pegtl::parse<hello::grammar, hello::action>(in, name);
+
     writer w;
-
-    try
-    {
-        w.write("idl3 compiler\n");
-    }
-    catch (std::exception const& e)
-    {
-        w.write("%\n", e.what());
-    }
-
+    w.write("Good bye, %!\n", name);
     w.flush_to_console();
 }
