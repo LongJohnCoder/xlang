4ba852bef7957f35705b8804e8385dc786851dfa
Harry Pierson <hpierson@microsoft.com>
2019-01-29 17:46:05 -0800

    WIP

--- src/tool/idl3/main.cpp
+++ src/tool/idl3/main.cpp
@@ -26,9 +26,38 @@ namespace hello
     };
 }
 
+namespace idl3
+{
+    struct ws : pegtl::one< ' ', '\t', '\n', '\r' > {};
+
+    template< typename R, typename P = ws >
+    struct padr : internal::seq< R, internal::star< P > > {};
+
+    struct NAMESPACE : pegtl::keyword<'n','a','m','e','s','p','a','c','e'>{};
+
+    struct OPEN_CURLY : padr<pegtl::one<'{'>>{};
+    struct CLOSE_CURLY : padr<pegtl::one<'}'>>{};
+
+    struct namespace_ : pegtl::seq<NAMESPACE, pegtl::identifier, OPEN_CURLY, CLOSE_CURLY>{};
+    struct grammar : pegtl::must<pegtl::star<ws>, pegtl::plus<namespace_>, pegtl::eof>{}; 
+
+    template<typename Rule>
+    struct action : pegtl::nothing<Rule>{};
+
+    template<>
+    struct action<pegtl::identifier>
+    {
+        template<typename Input>
+        static void apply(Input const& in, std::string& v)
+        {
+            v = in.string();
+        }
+    };
+}
+
 int main(int const /*argc*/, char** /*argv*/)
 {
-    pegtl::string_input<> in("Hello, Harry!", "from_content");
+    pegtl::string_input<> in("namespace ATest {}", "from_content");
 
     std::string name;
     pegtl::parse<hello::grammar, hello::action>(in, name);
