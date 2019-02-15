af1a85c19510b9e73f12652d6f6f89bf5750f2f0
Harry Pierson <hpierson@microsoft.com>
2019-02-15 14:08:45 -0800

    updated keywords

--- src/tool/cdl/main.cpp
+++ src/tool/cdl/main.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include <tao/pegtl/contrib/parse_tree.hpp>
 #include <tao/pegtl/analyze.hpp>
+#include <tao/pegtl/contrib/raw_string.hpp>
 
 struct writer : xlang::text::writer_base<writer>
 {
@@ -179,50 +180,232 @@ namespace cdl
             identifier_or_keyword > >{};
 
     // 2.4.3 Keywords
-    struct keyword : peg::sor< 
-        peg::string< 'a', 'b', 's', 't', 'r', 'a', 'c', 't' >, 
-        peg::string< 'a', 't', 't', 'r', 'i', 'b', 'u', 't', 'e' >, 
-        peg::string< 'B', 'o', 'o', 'l', 'e', 'a', 'n' >, 
-        peg::string< 'C', 'h', 'a', 'r' >, 
-        peg::string< 'c', 'l', 'a', 's', 's' >, 
-        peg::string< 'c', 'o', 'n', 's', 't' >, 
-        peg::string< 'd', 'e', 'f', 'a', 'u', 'l', 't' >, 
-        peg::string< 'd', 'e', 'l', 'e', 'g', 'a', 't', 'e' >, 
-        peg::string< 'D', 'o', 'u', 'b', 'l', 'e' >, 
-        peg::string< 'e', 'n', 'u', 'm' >, 
-        peg::string< 'e', 'v', 'e', 'n', 't' >, 
-        peg::string< 'f', 'a', 'l', 's', 'e' >, 
-        peg::string< 'G', 'u', 'i', 'd' >, 
-        peg::string< 'i', 'n' >, 
-        peg::string< 'I', 'n', 't', '8' >, 
-        peg::string< 'I', 'n', 't', '1', '6' >, 
-        peg::string< 'I', 'n', 't', '3', '2' >, 
-        peg::string< 'I', 'n', 't', '6', '4' >, 
-        peg::string< 'i', 'n', 't', 'e', 'r', 'f', 'a', 'c', 'e' >, 
-        peg::string< 'l', 'o', 'n', 'g' >, 
-        peg::string< 'n', 'a', 'm', 'e', 's', 'p', 'a', 'c', 'e' >, 
-        peg::string< 'n', 'u', 'l', 'l' >, 
-        peg::string< 'O', 'b', 'j', 'e', 'c', 't' >, 
-        peg::string< 'o', 'u', 't' >, 
-        peg::string< 'o', 'v', 'e', 'r', 'r', 'i', 'd', 'e' >, 
-        peg::string< 'p', 'a', 'r', 't', 'i', 'a', 'l' >, 
-        peg::string< 'p', 'r', 'o', 't', 'e', 'c', 't', 'e', 'd' >, 
-        peg::string< 'p', 'u', 'b', 'l', 'i', 'c' >, 
-        peg::string< 'r', 'e', 'f' >, 
-        peg::string< 's', 'e', 'a', 'l', 'e', 'd' >, 
-        peg::string< 's', 'h', 'o', 'r', 't' >, 
-        peg::string< 's', 't', 'a', 't', 'i', 'c' >, 
-        peg::string< 'S', 't', 'r', 'i', 'n', 'g' >, 
-        peg::string< 's', 't', 'r', 'u', 'c', 't' >, 
-        peg::string< 'S', 'i', 'n', 'g', 'l', 'e' >, 
-        peg::string< 't', 'r', 'u', 'e' >, 
-        peg::string< 'U', 'I', 'n', 't', '8' >, 
-        peg::string< 'U', 'I', 'n', 't', '1', '6' >, 
-        peg::string< 'U', 'I', 'n', 't', '3', '2' >, 
-        peg::string< 'U', 'I', 'n', 't', '6', '4' >, 
-        peg::string< 'u', 's', 'i', 'n', 'g' >, 
-        peg::string< 'v', 'i', 'r', 't', 'u', 'a', 'l' >, 
-        peg::string< 'v', 'o', 'i', 'd' > > {};
+
+    struct str_abstract : TAO_PEGTL_STRING( "abstract" ){};
+    struct str_add : TAO_PEGTL_STRING( "add" ){};
+    struct str_assembly : TAO_PEGTL_STRING( "assembly" ){};
+    struct str_attribute : TAO_PEGTL_STRING( "attribute" ){};
+    struct str_Boolean : TAO_PEGTL_STRING( "Boolean" ){};
+    struct str_Char : TAO_PEGTL_STRING( "Char" ){};
+    struct str_class : TAO_PEGTL_STRING( "class" ){};
+    struct str_const : TAO_PEGTL_STRING( "const" ){};
+    struct str_default : TAO_PEGTL_STRING( "default" ){};
+    struct str_define : TAO_PEGTL_STRING( "define" ){};
+    struct str_delegate : TAO_PEGTL_STRING( "delegate" ){};
+    struct str_disable : TAO_PEGTL_STRING( "disable" ){};
+    struct str_Double : TAO_PEGTL_STRING( "Double" ){};
+    struct str_elif : TAO_PEGTL_STRING( "elif" ){};
+    struct str_else : TAO_PEGTL_STRING( "else" ){};
+    struct str_endif : TAO_PEGTL_STRING( "endif" ){};
+    struct str_endregion : TAO_PEGTL_STRING( "endregion" ){};
+    struct str_enum : TAO_PEGTL_STRING( "enum" ){};
+    struct str_error : TAO_PEGTL_STRING( "error" ){};
+    struct str_event : TAO_PEGTL_STRING( "event" ){};
+    struct str_false : TAO_PEGTL_STRING( "false" ){};
+    struct str_field : TAO_PEGTL_STRING( "field" ){};
+    struct str_get : TAO_PEGTL_STRING( "get" ){};
+    struct str_Guid : TAO_PEGTL_STRING( "Guid" ){};
+    struct str_hidden : TAO_PEGTL_STRING( "hidden" ){};
+    struct str_if : TAO_PEGTL_STRING( "if" ){};
+    struct str_in : TAO_PEGTL_STRING( "in" ){};
+    struct str_Int16 : TAO_PEGTL_STRING( "Int16" ){};
+    struct str_Int32 : TAO_PEGTL_STRING( "Int32" ){};
+    struct str_Int64 : TAO_PEGTL_STRING( "Int64" ){};
+    struct str_Int8 : TAO_PEGTL_STRING( "Int8" ){};
+    struct str_interface : TAO_PEGTL_STRING( "interface" ){};
+    struct str_internal : TAO_PEGTL_STRING( "internal" ){};
+    struct str_line : TAO_PEGTL_STRING( "line" ){};
+    struct str_long : TAO_PEGTL_STRING( "long" ){};
+    struct str_method : TAO_PEGTL_STRING( "method" ){};
+    struct str_module : TAO_PEGTL_STRING( "module" ){};
+    struct str_namespace : TAO_PEGTL_STRING( "namespace" ){};
+    struct str_new : TAO_PEGTL_STRING( "new" ){};
+    struct str_null : TAO_PEGTL_STRING( "null" ){};
+    struct str_Object : TAO_PEGTL_STRING( "Object" ){};
+    struct str_out : TAO_PEGTL_STRING( "out" ){};
+    struct str_override : TAO_PEGTL_STRING( "override" ){};
+    struct str_param : TAO_PEGTL_STRING( "param" ){};
+    struct str_partial : TAO_PEGTL_STRING( "partial" ){};
+    struct str_pragma : TAO_PEGTL_STRING( "pragma" ){};
+    struct str_private : TAO_PEGTL_STRING( "private" ){};
+    struct str_property : TAO_PEGTL_STRING( "property" ){};
+    struct str_protected : TAO_PEGTL_STRING( "protected" ){};
+    struct str_public : TAO_PEGTL_STRING( "public" ){};
+    struct str_ref : TAO_PEGTL_STRING( "ref" ){};
+    struct str_region : TAO_PEGTL_STRING( "region" ){};
+    struct str_remove : TAO_PEGTL_STRING( "remove" ){};
+    struct str_restore : TAO_PEGTL_STRING( "restore" ){};
+    struct str_return : TAO_PEGTL_STRING( "return" ){};
+    struct str_sealed : TAO_PEGTL_STRING( "sealed" ){};
+    struct str_set : TAO_PEGTL_STRING( "set" ){};
+    struct str_short : TAO_PEGTL_STRING( "short" ){};
+    struct str_Single : TAO_PEGTL_STRING( "Single" ){};
+    struct str_static : TAO_PEGTL_STRING( "static" ){};
+    struct str_String : TAO_PEGTL_STRING( "String" ){};
+    struct str_struct : TAO_PEGTL_STRING( "struct" ){};
+    struct str_true : TAO_PEGTL_STRING( "true" ){};
+    struct str_type : TAO_PEGTL_STRING( "type" ){};
+    struct str_UInt16 : TAO_PEGTL_STRING( "UInt16" ){};
+    struct str_UInt32 : TAO_PEGTL_STRING( "UInt32" ){};
+    struct str_UInt64 : TAO_PEGTL_STRING( "UInt64" ){};
+    struct str_UInt8 : TAO_PEGTL_STRING( "UInt8" ){};
+    struct str_undef : TAO_PEGTL_STRING( "undef" ){};
+    struct str_using : TAO_PEGTL_STRING( "using" ){};
+    struct str_virtual : TAO_PEGTL_STRING( "virtual" ){};
+    struct str_void : TAO_PEGTL_STRING( "void" ){};
+    struct str_warning : TAO_PEGTL_STRING( "warning" ){};
+
+    template< typename Key >
+    struct key : peg::seq< Key, peg::not_at< peg::identifier_other > > {};
+
+    struct key_abstract : key< str_abstract > {};
+    struct key_add : key< str_add > {};
+    struct key_assembly : key< str_assembly > {};
+    struct key_attribute : key< str_attribute > {};
+    struct key_Boolean : key< str_Boolean > {};
+    struct key_Char : key< str_Char > {};
+    struct key_class : key< str_class > {};
+    struct key_const : key< str_const > {};
+    struct key_default : key< str_default > {};
+    struct key_define : key< str_define > {};
+    struct key_delegate : key< str_delegate > {};
+    struct key_disable : key< str_disable > {};
+    struct key_Double : key< str_Double > {};
+    struct key_elif : key< str_elif > {};
+    struct key_else : key< str_else > {};
+    struct key_endif : key< str_endif > {};
+    struct key_endregion : key< str_endregion > {};
+    struct key_enum : key< str_enum > {};
+    struct key_error : key< str_error > {};
+    struct key_event : key< str_event > {};
+    struct key_false : key< str_false > {};
+    struct key_field : key< str_field > {};
+    struct key_get : key< str_get > {};
+    struct key_Guid : key< str_Guid > {};
+    struct key_hidden : key< str_hidden > {};
+    struct key_if : key< str_if > {};
+    struct key_in : key< str_in > {};
+    struct key_Int16 : key< str_Int16 > {};
+    struct key_Int32 : key< str_Int32 > {};
+    struct key_Int64 : key< str_Int64 > {};
+    struct key_Int8 : key< str_Int8 > {};
+    struct key_interface : key< str_interface > {};
+    struct key_internal : key< str_internal > {};
+    struct key_line : key< str_line > {};
+    struct key_long : key< str_long > {};
+    struct key_method : key< str_method > {};
+    struct key_module : key< str_module > {};
+    struct key_namespace : key< str_namespace > {};
+    struct key_new : key< str_new > {};
+    struct key_null : key< str_null > {};
+    struct key_Object : key< str_Object > {};
+    struct key_out : key< str_out > {};
+    struct key_override : key< str_override > {};
+    struct key_param : key< str_param > {};
+    struct key_partial : key< str_partial > {};
+    struct key_pragma : key< str_pragma > {};
+    struct key_private : key< str_private > {};
+    struct key_property : key< str_property > {};
+    struct key_protected : key< str_protected > {};
+    struct key_public : key< str_public > {};
+    struct key_ref : key< str_ref > {};
+    struct key_region : key< str_region > {};
+    struct key_remove : key< str_remove > {};
+    struct key_restore : key< str_restore > {};
+    struct key_return : key< str_return > {};
+    struct key_sealed : key< str_sealed > {};
+    struct key_set : key< str_set > {};
+    struct key_short : key< str_short > {};
+    struct key_Single : key< str_Single > {};
+    struct key_static : key< str_static > {};
+    struct key_String : key< str_String > {};
+    struct key_struct : key< str_struct > {};
+    struct key_true : key< str_true > {};
+    struct key_type : key< str_type > {};
+    struct key_UInt16 : key< str_UInt16 > {};
+    struct key_UInt32 : key< str_UInt32 > {};
+    struct key_UInt64 : key< str_UInt64 > {};
+    struct key_UInt8 : key< str_UInt8 > {};
+    struct key_undef : key< str_undef > {};
+    struct key_using : key< str_using > {};
+    struct key_virtual : key< str_virtual > {};
+    struct key_void : key< str_void > {};
+    struct key_warning : key< str_warning > {};
+
+    struct keyword : peg::sor<
+        str_abstract,
+        str_add,
+        str_assembly,
+        str_attribute,
+        str_Boolean,
+        str_Char,
+        str_class,
+        str_const,
+        str_default,
+        str_define,
+        str_delegate,
+        str_disable,
+        str_Double,
+        str_elif,
+        str_else,
+        str_endif,
+        str_endregion,
+        str_enum,
+        str_error,
+        str_event,
+        str_false,
+        str_field,
+        str_get,
+        str_Guid,
+        str_hidden,
+        str_if,
+        str_in,
+        str_Int16,
+        str_Int32,
+        str_Int64,
+        str_Int8,
+        str_interface,
+        str_internal,
+        str_line,
+        str_long,
+        str_method,
+        str_module,
+        str_namespace,
+        str_new,
+        str_null,
+        str_Object,
+        str_out,
+        str_override,
+        str_param,
+        str_partial,
+        str_pragma,
+        str_private,
+        str_property,
+        str_protected,
+        str_public,
+        str_ref,
+        str_region,
+        str_remove,
+        str_restore,
+        str_return,
+        str_sealed,
+        str_set,
+        str_short,
+        str_Single,
+        str_static,
+        str_String,
+        str_struct,
+        str_true,
+        str_type,
+        str_UInt16,
+        str_UInt32,
+        str_UInt64,
+        str_UInt8,
+        str_undef,
+        str_using,
+        str_virtual,
+        str_void,
+        str_warning > {};
 
     // 2.4.4 Literals
     struct boolean_literal; 
